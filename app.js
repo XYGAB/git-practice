fb990ef12c35dbb84af6add7932b834d2581612a
unknown <gabsslemiabaya@gmail.com>
2024-09-12 21:06:50 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-let name = "BSCS SECOND YEAR";
+let name = "bscs 2c";
 console.log(name);
+
+const channelName = "pogiii";
+console.log(channelName);
+
+const randomNumber = 28;
+console.log(randomNumber);
